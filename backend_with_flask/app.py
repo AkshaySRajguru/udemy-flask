4da2d0f6a8bd8cef6da48041ca435f440e1f4d51
Akshay Rajguru <akshaygururaj141@gmail.com>
2022-08-05 00:12:10 +0530

    render templating

--- backend_with_flask/app.py
+++ backend_with_flask/app.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return {"message": "Hello World!"}
+
+@app.route("/home")
+def get_home():
+    return render_template("index.html")
+
+@app.route("/second")
+def get_second():
+    return render_template("second.html")
