46ca77f800a8017a84a150a5805774640d225694
Akshay Rajguru <akshaygururaj141@gmail.com>
2022-08-06 10:23:17 +0530

    jinja 2

--- backend_with_flask/app.py
+++ backend_with_flask/app.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -8,7 +9,7 @@ def hello_world():
 
 @app.route("/home")
 def get_home():
-    return render_template("index.html")
+    return render_template("index.html", name="Akshay", template_name = "Taa Daa !")
 
 @app.route("/second")
 def get_second():
